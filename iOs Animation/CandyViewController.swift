05258353f40b88ffd73bb452cfa10d9a51cc55a4
Ziabloff <zwxif35@gmail.com>
2022-02-08 19:27:44 +0300

    animation

---
+++ iOs Animation/CandyViewController.swift
@@ -0,0 +1,38 @@
+//
+//  CandyViewController.swift
+//  iOs Animation
+//
+//  Created by Student on 08.02.2022.
+//
+
+import UIKit
+
+class CandyViewController: UIViewController {
+
+    @IBOutlet weak var candyPicture: UIImageView!
+    
+    override func viewWillAppear(_ animated: Bool) {
+        UIView.animate(withDuration: 1.0, delay: 1.0, options: [.repeat,.autoreverse], animations: {
+            self.candyPicture.frame.size.width = 105
+            self.candyPicture.frame.size.height = 54
+        }, completion: nil)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
